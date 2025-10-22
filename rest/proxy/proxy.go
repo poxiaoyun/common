f81cf853b075bcf725875e82fd398f1bc1d6eb76
cnfatal <cnfatal@gmail.com>
2025-10-22 08:34:52 +0000

    feat(proxy): customizable ErrorResponder

--- rest/proxy/proxy.go
+++ rest/proxy/proxy.go
@@ -13,6 +13,7 @@ import (
 	liberrors "xiaoshiai.cn/common/errors"
 	"xiaoshiai.cn/common/httpclient"
 	"xiaoshiai.cn/common/log"
+	"xiaoshiai.cn/common/meta"
 	"xiaoshiai.cn/common/rest/api"
 )
 
@@ -33,6 +34,8 @@ type Proxy struct {
 	// this is useful when you want to add a prefix to the modified html
 	// set ProxyPrefix to '/proxy'
 	ProxyPrefix string
+
+	ErrorResponser ErrorResponder
 }
 
 func (p Proxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -42,12 +45,12 @@ func (p Proxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 			pr.Out.URL.Path = requestpath
 			pr.SetURL(p.ClientConfig.Server)
 		},
-		Transport: p.ClientConfig.RoundTripper,
-		ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
-			http.Error(w, err.Error(), http.StatusBadGateway)
-		},
+		Transport:    p.ClientConfig.RoundTripper,
+		ErrorHandler: meta.Or[ErrorResponder](p.ErrorResponser, DefaultErrorResponder{}).Error,
+		// for websocket support
+		FlushInterval: -1,
 	}
-	prefix := strings.TrimSuffix(req.URL.Path, requestpath)
+	var prefix string
 	if proxyRequestURI, _ := url.ParseRequestURI(req.Header.Get("X-Forwarded-Uri")); proxyRequestURI != nil {
 		// is this request from a proxy?
 		// if request is from a proxy, the X-Forwarded-Uri header will be set
@@ -57,7 +60,7 @@ func (p Proxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		prefix = strings.TrimSuffix(proxyRequestURI.Path, requestpath)
 	} else {
 		// /proxy is our front end proxy path
-		prefix = filepath.Join(p.ProxyPrefix, prefix)
+		prefix = filepath.Join(p.ProxyPrefix, strings.TrimSuffix(req.URL.Path, requestpath))
 	}
 	rp.Transport = &Transport{
 		PathPrepend:  prefix,
@@ -67,9 +70,15 @@ func (p Proxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	rp.ServeHTTP(w, req)
 }
 
-type ErrorResponser struct{}
+type ErrorResponder interface {
+	Error(w http.ResponseWriter, req *http.Request, err error)
+}
 
-func (ErrorResponser) Error(w http.ResponseWriter, req *http.Request, err error) {
+var _ ErrorResponder = DefaultErrorResponder{}
+
+type DefaultErrorResponder struct{}
+
+func (DefaultErrorResponder) Error(w http.ResponseWriter, req *http.Request, err error) {
 	dnse := &net.DNSError{}
 	if errors.As(err, &dnse) {
 		err = liberrors.NewServiceUnavailable(err.Error())
